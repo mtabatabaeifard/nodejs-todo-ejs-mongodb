36f34923378236e59c3d219b8763f9ca6085b916
Seyed Mohammadhossein Tabatabaeifard <mohammadirani469@gmail.com>
2023-04-09 17:55:00 +0330

    added important note for model right config!

--- models/Todo.js
+++ models/Todo.js
@@ -6,3 +6,10 @@ const Schema = mongoose.Schema({title:String , completed:{
 const Todo = mongoose.model('Todo',Schema);
 
 module.exports = Todo;
+
+
+//  !! important note:
+// ? Right Model Config in mongoose :
+// ! first define Shcema!
+// * Then Create Model
+// ? Then export the model...
